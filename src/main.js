165252f1c1cb6f70f911a0bd89a7c4b50deccd0b
xws <1115958321@qq.com>
2019-07-27 21:16:50 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
+import Vant from 'vant'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'vant/lib/index.css'
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
